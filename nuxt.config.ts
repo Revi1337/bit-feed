cee631ad2e9f467b71cec87fe091206dba058471
Revi1337 <david122123@gmail.com>
2026-06-11 22:09:25 +0900

    chore: set default color mode to dark

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
+    preference: 'dark',
+    fallback: 'dark',
     classSuffix: ''
   },
   srcDir: 'app/',
